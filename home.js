73221dfec81340ee529eecb6ca61fe621ef1a41d
Cinemamo <mananabas4578@gmail.com>
2025-05-12 06:12:11 +0800

    Update home.js

--- home.js
+++ home.js
@@ -43,6 +43,6 @@ document.getElementById("closeModal").onclick = () => {
 };
 
 // Load sections
-fetchMovies(`https://api.themoviedb.org/3/trending/movie/day?api_key=${27077b0de31705d036e5367a680c8d5f}`, trendingContainer);
-fetchMovies(`https://api.themoviedb.org/3/movie/top_rated?api_key=${27077b0de31705d036e5367a680c8d5f}`, topImdbContainer);
-fetchMovies(`https://api.themoviedb.org/3/movie/now_playing?api_key=${27077b0de31705d036e5367a680c8d5f}`, newReleaseContainer);
+fetchMovies(`https://api.themoviedb.org/3/trending/movie/day?api_key=${apikey}`, trendingContainer);
+fetchMovies(`https://api.themoviedb.org/3/movie/top_rated?api_key=${apikey}`, topImdbContainer);
+fetchMovies(`https://api.themoviedb.org/3/movie/now_playing?api_key=${apikey}`, newReleaseContainer);
